77b3aa1a28b6dc5e3e62cd4c5b75478894ca02e3
catfo <pranavlsgreat210120@gmail.com>
2024-02-18 15:22:17 +0530

    refactor: removed node_modules from git cache

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+const users = ["Rahul","Messi","Federer"]
+
+app.use(cors())
+
+app.get("/users", (req,res) => {
+    res.json({users : users})
+})
+
+app.post("/users", (req,res) => {
+    console.log(req.body)
+})
+
+app.listen(PORT,() => {
+    console.log(`Server is running on ${PORT}`)
+})
