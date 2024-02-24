6ecdbbf012a35e4e495751bfedb9c7d04589d3da
catfo <pranavlsgreat210120@gmail.com>
2024-02-24 20:38:52 +0530

    style: shifted the import statements to the top

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import { WebSocketServer } from "ws";
-const server = new WebSocketServer({ port : "5000" })
-
 import express from "express";
 import cors from "cors"
+
+const server = new WebSocketServer({ port : "5000" })
 
 server.on("connection", socket => {
     console.log("user connnected");
