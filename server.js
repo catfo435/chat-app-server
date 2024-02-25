4beec51093f0a75a5de7c02d972583661be8b9e0
catfo <pranavlsgreat210120@gmail.com>
2024-02-25 16:48:08 +0530

    feat: New API route in dev, /getmessages

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ import cors from "cors"
 import pg from 'pg'
 import bodyParser from 'body-parser';
 
+const db = new pg.Pool({
+    user: 'postgres',
+    password: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'chatapp'
+});
+
 
 const server = new WebSocketServer({ port : "5000" })
 
@@ -24,34 +32,45 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
+app.use(bodyParser.json())
 
-app.get("/", async (req,res) => {
-    res.send("API!")
-})
-
-app.post("/authenticate",bodyParser.json(), async (req,res) => {
-
+app.get("/getmessages", async (req,res) => {
     try {
-
-        const client = new pg.Client({
-            user: 'postgres',
-            password: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            database: 'postgres'
-        });
-
-        await client.connect()
-
-        const result = await client.query(`SELECT * FROM users WHERE username = $1::text`,[req.body.username])
-        if (result.rows[0] && result.rows[0].pass == req.body.password) res.status(200).send("Authentication approved!")
-        else res.status(401).send("Authentication Error")
-
-        await client.end()
+        const result = await db.query(`SELECT * FROM transaction01`)
+        res.status(200).send({messages:result.rows})
       } 
     catch (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
+    }
+})
+
+app.post("/authenticate", async (req,res) => {
+
+    try {
+        const result = await db.query(`SELECT * FROM users WHERE username = $1::text`,[req.body.username])
+        if (result.rows[0] && result.rows[0].pass == req.body.password) res.status(200).send("Authentication approved!")
+        else res.status(401).send("Authentication Error")
+      } 
+    catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+app.post("/newuser", async (req,res) => {
+
+    res.status(501).send("Not Implemented Yet")
+    return; // until user addition "+" is implemented
+
+    try {
+        await db.query(`INSERT INTO users(username,pass,secret) VALUES($1,$2,$3)`,[req.body.username,req.body.password,"abcd"])
+        res.status(200).send("Account Made!")
+      } 
+    catch (err) {
+        console.error(err);
+        if (err.code == '23505') res.status(403).send('Username already exists')
+        else res.status(500).send('Internal Server Error');
     }
 })
 
