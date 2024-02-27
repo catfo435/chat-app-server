55fb4bafe6eae60802b690d9b1417b5ee080a359
catfo <pranavlsgreat210120@gmail.com>
2024-02-28 02:22:49 +0530

    feat: Authentication completed(Add users is pending)

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ import { createServer } from "http";
 import express from "express";
 import cors from "cors"
 import pg from 'pg'
-import bodyParser from 'body-parser';
+
+import jwt from 'jsonwebtoken'
+
+import 'dotenv/config'
 
 const db = new pg.Pool({
     user: 'postgres',
@@ -21,7 +24,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-app.use(bodyParser.json())
+app.use(express.json())
 
 const httpServer = createServer(app);
 const io = new Server(httpServer,{
@@ -30,16 +33,17 @@ const io = new Server(httpServer,{
 
 io.use((socket, next) => {
     const username = socket.handshake.auth.username;
-    socket.id = username;
+    socket.user = username;
     next();
   });
 
 io.on("connection", socket => {
     console.log(`User ${socket.handshake.auth.username} connnected`);
+    socket.join(socket.user);
 
     socket.on("message", async ({ message,sender,receiver}) => {
         await db.query(`INSERT INTO transaction01(message,sender,receiver) VALUES($1,$2,$3)`,[message,sender,receiver])
-        socket.to(receiver).emit("message", {
+        socket.to(sender).to(receiver).emit("message", {
           message,
           sender,
           receiver
@@ -63,9 +67,15 @@ app.get("/messages", async (req,res) => {
 
 app.post("/users/login", async (req,res) => {
 
+    // bcrypt.compare(req.body.password, result.rows[0].pass, function(err, result) {
+    //     respond here
+    // })
+
+    const token = jwt.sign({ username: req.body.username }, process.env.SECRET_SALT, {expiresIn : "30m"})
+
     try {
         const result = await db.query(`SELECT * FROM users WHERE username = $1::text`,[req.body.username])
-        if (result.rows[0] && result.rows[0].pass == req.body.password) res.status(200).send("Authentication approved!")
+        if (result.rows[0] && result.rows[0].pass == req.body.password) res.status(200).send({token})
         else res.status(401).send("Authentication Error")
       } 
     catch (err) {
@@ -79,15 +89,18 @@ app.post("/users", async (req,res) => {
     res.status(501).send("Not Implemented Yet")
     return; // until user addition "+" is implemented
 
-    try {
-        await db.query(`INSERT INTO users(username,pass,secret) VALUES($1,$2,$3)`,[req.body.username,req.body.password,"abcd"])
-        res.status(200).send("Account Made!")
-      } 
-    catch (err) {
-        console.error(err);
-        if (err.code == '23505') res.status(403).send('Username already exists')
-        else res.status(500).send('Internal Server Error');
-    }
+    bcrypt.hash(req.body.password, salt, async (err, hash) => {
+        try {
+        
+            await db.query(`INSERT INTO users(username,pass,secret) VALUES($1,$2,$3)`,[req.body.username,hash,"abcd"])
+            res.status(200).send("Account Made!")
+          } 
+        catch (err) {
+            console.error(err);
+            if (err.code == '23505') res.status(403).send('Username already exists')
+            else res.status(500).send('Internal Server Error');
+        }
+    })
 })
 
 httpServer.listen(4000,() => {
